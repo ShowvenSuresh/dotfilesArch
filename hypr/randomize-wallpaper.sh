f3961c470b5aa4a796e5b11dcfe42eb0c9512219
ShowvenSuresh <showven1201@gmail.com>
2026-01-19 23:36:37 +0800

    finally doneeee

--- hypr/randomize-wallpaper.sh
+++ hypr/randomize-wallpaper.sh
@@ -1,10 +1,18 @@
+
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="/home/thinker12/.config/hypr/wallpaper/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+WALLPAPER_DIR="$HOME/.config/hypr/wallpaper"
 
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# Get currently active wallpaper path
+CURRENT_WALL=$(hyprctl hyprpaper listloaded | awk '{print $2}')
 
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+# Pick a random wallpaper different from current
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | grep -v "$CURRENT_WALL" | shuf -n 1)
+
+# Safety check
+[ -z "$WALLPAPER" ] && exit 1
+
+# Apply wallpaper
+hyprctl hyprpaper preload "$WALLPAPER"
+hyprctl hyprpaper wallpaper ",$WALLPAPER"
+
